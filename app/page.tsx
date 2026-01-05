bdec8991e7216913070a3b49b366d9840ec13c9b
Keerthana Sunder <keerubarbie@gmail.com>
2026-01-04 19:10:55 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         {`I'm Keerthana Sunder, in short - Keer. This page is my open space to write about stuff that interest me. 
-        
+        <br />
         I'm a software engineer by profession, so I'm making an habit to read industry research papers and wirte my thoughts on them, because when I was a kid, my parents used to say - writing something instead of plain reading increases the remembrance power by 10 times and I believe that! 
-
+        <br />
         Apart from that, I write poetry. I'm planning to move poetry writing from instagram to this.
-
+        <br />
         I also am keeping another section for writing about other stuff, prolly about my good and rough days, hobbies, music, maybe life? 
         `}
       </p>
