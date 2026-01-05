c4815377868fefdf9a35bea624d96bab43436307
Keerthana Sunder <keerubarbie@gmail.com>
2026-01-04 19:08:33 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,14 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm Keerthana Sunder, in short - Keer. This page is my open space to write about stuff that interest me. 
+        
+        I'm a software engineer by profession, so I'm making an habit to read industry research papers and wirte my thoughts on them, because when I was a kid, my parents used to say - writing something instead of plain reading increases the remembrance power by 10 times and I believe that! 
+
+        Apart from that, I write poetry. I'm planning to move poetry writing from instagram to this.
+
+        I also am keeping another section for writing about other stuff, prolly about my good and rough days, hobbies, music, maybe life? 
+        `}
       </p>
       <div className="my-8">
         <BlogPosts />
